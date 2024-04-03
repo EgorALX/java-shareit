1d973937e5dc99aa54e759baedbe62c8371adb75
Egor <alexanoffegor@yandex.ru>
2024-04-03 18:52:34 +0300

    \('U')/

--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.exception.model.NotFoundException;
-import ru.practicum.shareit.exception.model.ValidationException;
 import ru.practicum.shareit.user.dto.UserCreateDto;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserUpdateDto;
