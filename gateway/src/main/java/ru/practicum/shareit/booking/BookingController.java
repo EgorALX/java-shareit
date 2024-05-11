b2a9be13d0cc44717c54fc73649b75dbe8e65dbd
Egor <alexanoffegor@yandex.ru>
2024-05-11 23:11:08 +0300

    first

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -11,6 +11,7 @@ import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import ru.practicum.shareit.booking.dto.BookingState;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 
@@ -20,7 +21,10 @@ import javax.validation.constraints.PositiveOrZero;
 @Slf4j
 @Validated
 public class BookingController {
+
 	private final BookingClient bookingClient;
+
+	private static final String USER_ID_HEADER = "X-Sharer-User-Id";
 
 	@GetMapping
 	public ResponseEntity<Object> getBookings(@RequestHeader("X-Sharer-User-Id") long userId,
@@ -45,4 +49,25 @@ public class BookingController {
 			@PathVariable Long bookingId) {
 		log.info("Get booking {}, userId={}", bookingId, userId);
 		return bookingClient.getBooking(userId, bookingId);
-	}}
+	}
+
+	@GetMapping("/owner")
+	public ResponseEntity<Object> getAllByOwner(@RequestHeader(USER_ID_HEADER) long userId,
+												@RequestParam(name = "state", defaultValue = "all") String stateParam,
+												@RequestParam(defaultValue = "0") @Min(0) int from,
+												@RequestParam(defaultValue = "10") @Min(1) int size) {
+		BookingState state = BookingState.from(stateParam)
+				.orElseThrow(() -> new IllegalArgumentException("Unknown state: " + stateParam));
+		log.info("Get booking for item owner with state {}, userId={}, from={}, size={}",
+				userId, stateParam, from, size);
+		return bookingClient.getAllForOwner(userId, state, from, size);
+	}
+
+	@PatchMapping("/{bookingId}")
+	public ResponseEntity<Object> confirmReject(@RequestHeader(USER_ID_HEADER) long userId,
+												@PathVariable long bookingId, @RequestParam String approved) {
+		log.info("Change state of booking with id {} by user with id {} to {}",
+				bookingId, userId, approved);
+		return bookingClient.confirmReject(userId, bookingId, approved);
+	}
+}
